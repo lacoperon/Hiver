b41fea4a24a14b4d782b91ecc7d2c74829dbb8da
lacoperon <elliot.james.williams@gmail.com>
2017-07-17 12:57:45 +0800

    Implemented spawn and creep detection (JOY! Proof of concept achieved)

--- src/main.ml
+++ src/main.ml
@@ -1,7 +1,39 @@
-let creeps : string array = [%bs.raw{|console.log({})|}]
+let creeps : string array = [%bs.raw{|Object.keys(Game.creeps)|}]
+let creepsArray = creeps
+let spawns : string array = [%bs.raw{|Object.keys(Game.spawns)|}]  (* Temporary Hard Coding *)
+let spawnsArray : string array = spawns
+
+
+type game
+external game : game = "Game" [@@bs.val]
+
+
+(* let spawnCreep : *)
+
+
+let iterateCreeps () : unit =
+  match Array.length creeps  with
+  | 0 -> Js.log("There are no creeps to iterate over") ;
+  | x -> Js.log("There are " ^ (string_of_int x) ^ " creeps currently") ;
+
+for i=0 to Array.length creeps - 1 do
+  (* Js.log(Array.get creeps i) *)
+  Js.log("One is named " ^ Array.get creeps i)
+done
+
+let iterateSpawns () : unit =
+  match Array.length spawns with
+  | 0 -> Js.log("There are no spawns to iterate over (should be untrue)") ;
+  | x -> Js.log("There are " ^ (string_of_int x) ^ " spawns currently") ;
+
+    for i=0 to Array.length spawns - 1 do
+      Js.log("One is named " ^ Array.get spawns i)
+    done
+
 
 let run () : unit =
-Js.log("TICK") ;
-Js.log(creeps)
+  ignore (let time : int = [%bs.raw{|Date.now()|}] in Js.log(time)) ;
+  ignore (iterateSpawns());
+  iterateCreeps()
 
-let doRun : unit = run()
+let runEachTick : unit = run()
