ef9c35e4741fc14df3b254b247b3211553103d90
lacoperon <elliot.james.williams@gmail.com>
2017-07-17 12:58:39 +0800

    Removed test file

--- test.js
+++
@@ -1,5 +0,0 @@
-console.log(Object.keys({}));
-console.log(`Non-Empty Case: ${Object.keys({key: "val"})}`);
-console.log(`Empty Array Case: ${[]}`);
-console.log(`Non-Empty Array Case: ${[1,2,3]}`);
-console.log([]);
