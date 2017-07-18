f5b4dec986b154cc07be637b0e33be89121f3525
lacoperon <elliot.james.williams@gmail.com>
2017-07-18 13:40:54 +0800

    Added basic screep spawning code, working around my JS currying issue via a helper module I'm writing in basic Javascript, even through I really prefer TypeScript by far

--- src/main.ml
+++ src/main.ml
@@ -1,14 +1,58 @@
 let creeps : string array = [%bs.raw{|Object.keys(Game.creeps)|}]
 let creepsArray = creeps
-let spawns : string array = [%bs.raw{|Object.keys(Game.spawns)|}]  (* Temporary Hard Coding *)
+(* let creepsObj : *)
+let spawns : string array = [%bs.raw{|Object.keys(Game.spawns)|}]
 let spawnsArray : string array = spawns
 
+type bodyPart =
+  | MOVE
+  | WORK
+  | CARRY
+  | ATTACK
+  | RANGED_ATTACK
+  | HEAL
+  | TOUGH
+  | CLAIM
 
-type game
-external game : game = "Game" [@@bs.val]
+
+external spawnCreepHelper : string -> string array -> unit = "" [@@bs.module "./supplemental", "Supplement"]
+
+let bodyPartToString(part : bodyPart) : string =
+  match part with
+  | MOVE -> "move" ;
+  | WORK -> "work" ;
+  | ATTACK -> "attack" ;
+  | CARRY -> "carry" ;
+  | RANGED_ATTACK -> "ranged_attack" ;
+  | TOUGH -> "tough" ;
+  | HEAL -> "heal" ;
+  | CLAIM -> "claim"
+
+let bodyPartToCost(part : bodyPart) : int =
+  match part with
+  | MOVE -> 50;
+  | WORK -> 100;
+  | ATTACK -> 80;
+  | CARRY -> 50;
+  | HEAL -> 250;
+  | RANGED_ATTACK -> 150;
+  | TOUGH -> 10;
+  | CLAIM -> 600
+
+let spawnCreep(spawn : string) (body : bodyPart array) : unit =
+  spawnCreepHelper (spawn) (Array.map bodyPartToString body) ;
+  Js.log("Spawning a new creep!")
 
 
-(* let spawnCreep : *)
+type roomPosition =
+  {
+    x : int;
+    y : int;
+    roomName : string
+  }
+
+
+
 
 
 let iterateCreeps () : unit =
@@ -27,13 +71,21 @@ let iterateSpawns () : unit =
   | x -> Js.log("There are " ^ (string_of_int x) ^ " spawns currently") ;
 
     for i=0 to Array.length spawns - 1 do
-      Js.log("One is named " ^ Array.get spawns i)
+      Js.log("One is named " ^ Array.get spawns i);
+      let body = [|WORK; CARRY; MOVE; MOVE|] in
+      spawnCreep(Array.get spawns i) body
     done
+
+(* external createCreep : (string, string array) -> unit =
+  "" [@bs.module "Game"] [@bs.scope spawns] [] *)
+
+
 
 
 let run () : unit =
   ignore (let time : int = [%bs.raw{|Date.now()|}] in Js.log(time)) ;
   ignore (iterateSpawns());
   iterateCreeps()
+  (* spawnCreep() *)
 
 let runEachTick : unit = run()
