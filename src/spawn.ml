8c80168d449479e10b8b104a3eda5f91d6479198
lacoperon <elliot.james.williams@gmail.com>
2017-07-22 15:03:41 +0800

    Added check to see if base body is too expensive to begin with

--- src/spawn.ml
+++ src/spawn.ml
@@ -21,6 +21,8 @@ let spawnCreepWithRole(spawn : string) (body : bodyPart array) (r : role) : int 
 let createLargestTandemBody(spawn : spawn)(body : bodyPart array) : bodyPart array =
   let spawnEnergy = getEnergyCapacity spawn in
   let bodyCost = arraySum (Array.map bodyPartToCost body) in
+  if spawnEnergy > bodyCost then
+    (
   let rec createLargestTandemBodyRec(spawnEnergyRemaining : int) (bodyUnit : bodyPart list) (currentBody : bodyPart list) : bodyPart array =
     if spawnEnergyRemaining < bodyCost then
       Array.of_list currentBody
@@ -28,3 +30,7 @@ let createLargestTandemBody(spawn : spawn)(body : bodyPart array) : bodyPart arr
       createLargestTandemBodyRec(spawnEnergyRemaining - bodyCost) (bodyUnit)(bodyUnit @ currentBody)
   in
   createLargestTandemBodyRec(spawnEnergy)(Array.to_list body)([])
+    )
+  else
+    (Js.log("Base body is too large");
+     body)
